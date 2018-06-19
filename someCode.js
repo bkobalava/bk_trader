16238a177a1c89b6309f1e7785399197f7b68b07
bkobalava <39155139+bkobalava@users.noreply.github.com>
2018-06-19 21:04:13 +0400

    Add files via upload

--- someCode.js
+++ someCode.js
@@ -69,7 +69,8 @@ var buyPrice = 0;
 var sellPrice = 0;
 var profit = 0;
 var jPrevCurr = 0;	
-var highA = [];  
+var 
+A = [];  
 var lowA = [];  
 var closeA = [];  
 
@@ -98,12 +99,16 @@ var lowA = [];  //low price
 var closeA = []; //close price  
 
 
-ticks.forEach(function(tick) {
-highA.push(tick[2]);  //high price  
-lowA.push(tick[3]);  //low price  
-closeA.push(tick[4]); //close price  
-});
-  
+// ticks.forEach(function(tick) {
+// highA.push(tick[2]);  //high price  
+// lowA.push(tick[3]);  //low price  
+// closeA.push(tick[4]); //close price  
+// });
+
+
+highA = [28.94, 28.38, 28.98, 29.16, 28.46, 27.9, 28.07, 27.65, 27.43, 27.17, 27, 27.2, 26.81, 27.18, 27.13, 26.97, 26.74, 26.58, 26.08, 25.32, 25.14, 25.08, 25.04, 25.06, 25.45, 25.23, 25.56, 26.05, 26.28, 26.16, 26.27, 26.03, 25.2, 25.25, 25.25, 25.37, 25.46, 25.33, 25.14, 24.58, 24.86, 24.63, 23.39, 23.39, 23.58, 23.54, 23.59, 23.46, 23.5, 23.54, 23.56, 23.54, 23.5, 23.56, 23.46, 23.62, 23.57, 23.38, 23.25, 23.18, 23.92, 23.87, 23.73, 23.66, 23.97, 24.1, 24.67, 25.33, 25.1, 25.16, 25.05, 24.73, 25.18, 25.55, 25.33, 25.95, 26.25, 26.67, 27.09, 26.5, 26.46, 26.19];
+lowA = [27.82, 27.3, 27.88, 28.06, 27.6, 26.83, 27.01, 26.86, 26.97, 26.68, 25.7, 26, 26.21, 26.41, 26.68, 26.05, 26.28, 25.85, 24.51, 24.78, 24.8, 24.52, 24.79, 24.75, 24.85, 24.93, 25.1, 25.08, 25.53, 25.7, 25.84, 24.97, 24.9, 25.02, 25.01, 25.04, 25.18, 25.02, 24.14, 24.21, 24.31, 23.2, 23.04, 22.73, 23.15, 23.25, 23.39, 23.22, 23.2, 23.33, 23.26, 23.34, 23.36, 23.39, 23.27, 23.31, 23.19, 23.14, 22.96, 22.76, 23.11, 23.58, 23.45, 23.3, 23.47, 23.75, 24, 24.55, 24.7, 24.78, 24.35, 24.37, 24.35, 25, 24.85, 25, 25.65, 25.88, 26.06, 26.02, 25.68, 25.91];
+closeA = [28.05, 28.18, 28.73, 28.59, 28.07, 27.47, 27.68, 27.14, 27.2, 27.09, 26.99, 26.36, 26.6, 27.05, 26.79, 26.38, 26.46, 26.08, 25.32, 25.03, 24.96, 24.95, 24.82, 24.88, 25.34, 25.19, 25.47, 26, 26, 26.13, 25.94, 25.13, 25.02, 25.19, 25.13, 25.31, 25.31, 25.04, 24.3, 24.49, 24.63, 23.28, 23.37, 23.3, 23.45, 23.44, 23.49, 23.35, 23.4, 23.4, 23.39, 23.44, 23.47, 23.42, 23.42, 23.48, 23.28, 23.21, 23, 23.16, 23.85, 23.72, 23.57, 23.64, 23.74, 23.91, 24.51, 24.93, 24.95, 24.85, 24.47, 24.5, 25.15, 25.41, 25.13, 25.94, 26.01, 26.56, 26.2, 26.3, 26.45, 25];
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
@@ -117,22 +122,63 @@ closeA.push(tick[4]); //close price
     var rsvs = [], ks = [], ds = [], js = [];
     var maximum, minimum;
     var lastK, lastD, curK, curD;
-	
-    for (var i = 0; i < ticks.length; i++) {
-      maximum = max(highA, i, 9);
-      minimum = min(lowA, i, 9);
+	var period = 9;
+    for (var i = 0; i < highA.length; i++) {
+      		if (i < period) {
+		// maximum = Math.max.apply(Math, highA.slice(0, i + 1));
+		maximum = Math.max.apply(i, highA.slice(0, i + 1));
+// console.log(i, maximum, highA.slice(0, i + 1));
+		}else{
+		// maximum = Math.max.apply(Math, highA.slice(i - period + 1, i + 1));
+		maximum = Math.max.apply(i, highA.slice(i - period + 1, i + 1));
+// console.log(i, maximum, highA.slice(i - period + 1, i + 1));
+		}
+
+				if (i < period) {
+		// minimum = Math.min.apply(Math, lowA.slice(0, i + 1));
+		minimum = Math.min.apply(i, lowA.slice(0, i + 1));
+// console.log(i, minimum, lowA.slice(0, i + 1));
+		}else{
+		// minimum = Math.min.apply(Math, lowA.slice(i - period + 1, i + 1));
+		minimum = Math.min.apply(i, lowA.slice(i - period + 1, i + 1));
+// console.log(i, minimum, lowA.slice(i - period + 1, i + 1));
+		}
+
 	  
       if (maximum == minimum) {
         rsvs.push(0);
       } else {
         rsvs.push((closeA[i] - minimum) / (maximum - minimum) * 100);
       }
-	}
+
+
+		if (i < period - 1) {
+		ks.push(0);
+		ds.push(0);
+		js.push(0);
+		} else if (i == period - 1) {
+		ks.push(50);
+		ds.push(50);
+		js.push(50);
+		} else {
+		ks.push((2 / 3 * ks[ks.length - 1]) + (1 / 3 * rsvs[i]));
+		ds.push((2 * ds[ds.length-1] / 3) + (ks[i] / 3));
+		js.push((3 * ks[i]) - (2 * ds[i]));
+		}
+// console.log(minimum, maximum, closeA[i], rsvs[i], ks[ks.length - 1])
+
+
+
+
+
+	  }
 	
-	ks = sma(rsvs, 3)
-	ds = sma(ks, 3)
-    for (var i = 0; i < ks.length; i++) js.push(3 * ks[i] - 2 * ds[i]);
-	  
+    for (var i = 0; i < rsvs.length; i++) {
+console.log(ks[i], ds[i], js[i]);
+	}
+    
+console.log(js.slice(js.length-4, js.length-1));
+
 var output = js.slice(js.length-4, js.length-1) // ძალიან ძველი, ძველი, წინა, ახალი (მერყევი)
 
 
@@ -213,104 +259,3 @@ fs.appendFile('./bk_trader/register.txt',sellPrice + "\t" + jOld + "\t" + jLast 
 }, 30000);
 
 //////////////////////////////////////////////////////////////////////////////////
-function kdj(ticks) {
-    var high = [], low = [], close = [];
-    var rsvs = [], ks = [], ds = [], js = [];
-    var maximum, minimum;
-    var lastK, lastD, curK, curD;
-	
-    for (var i = 0; i < ticks.length; i++) {
-	  high.push(ticks[i][0]);	
-	  low.push(ticks[i][1]);	
-	  close.push(ticks[i][2]);
-	}
-	
-    for (var i = 0; i < ticks.length; i++) {
-      maximum = max(high, i, 9);
-      minimum = min(low, i, 9);
-	  
-      if (maximum == minimum) {
-        rsvs.push(0);
-      } else {
-        rsvs.push((close[i] - minimum) / (maximum - minimum) * 100);
-      }
-	}
-
-
-	
-	ks = sma(rsvs, 3)
-	ds = sma(ks, 3)
-    for (var i = 0; i < ks.length; i++) js.push(3 * ks[i] - 2 * ds[i]);
-	  
-var output = js.slice(js.length-4, js.length-1) // ძალიან ძველი, ძველი, წინა, ახალი (მერყევი)
-// var output = js.slice(0, 20)
-	// return {"k": ks, "d": ds, "j": js};
-console.log(js);
-    return output;
-  };
-  
-//////////////////////////////////////////////////////////////////////////////////
-function sma(arr, num) {
-  if (!Array.isArray(arr)) {
-    throw TypeError('expected first argument to be an array');
-  }
-
-  var res = [];
-    for (var n = 0; n < arr.length; n++) {
-		if ((n + num) > (arr.length - 1)) {
-		var avg = arr.slice(n, (arr.length)).reduce(function(acc, val) { return acc + val; }, 0) / (arr.length - n);
-		}else{
-		var avg = arr.slice(n, (n + num)).reduce(function(acc, val) { return acc + val; }, 0) / num;
-		}
-		res.push(avg);
-  }
-  return res;
-}
-
-//////////////////////////////////////////////////////////////////////////////////
-function max(arr, n, num) {
-  if (!Array.isArray(arr)) {
-    throw TypeError('expected first argument to be an array');
-  }
-
-  var res = [];
-		if ((n + num) > (arr.length - 1)) {
-		// res.push(Math.max(arr.slice(n, (arr.length - 1))));
-		// res.push(arr.slice(n, (arr.length - 1)).reduce(function(acc, val) { return acc + val; }, 0));
-		// res.push(Math.max.apply(Math, arr.slice(n, (arr.length))));
-		return Math.max.apply(Math, arr.slice(n, (arr.length)));
-		}else{
-		// res.push(Math.max(arr.slice(n, (n + num))));
-		// res.push(arr.slice(n, (n + num)).reduce(function(acc, val) { return acc + val; }, 0));
-		// res.push(Math.max.apply(Math, arr.slice(n, (n + num))));
-		return Math.max.apply(Math, arr.slice(n, (n + num)));
-		}
-		// res.push(maximum);
-  // return res;
-}
-//////////////////////////////////////////////////////////////////////////////////
-function min(arr, n, num) {
-  if (!Array.isArray(arr)) {
-    throw TypeError('expected first argument to be an array');
-  }
-
-  var res = [];
-		if ((n + num) > (arr.length - 1)) {
-		// res.push(Math.min.apply(Math, arr.slice(n, (arr.length))));
-		return Math.min.apply(Math, arr.slice(n, (arr.length)));
-		}else{
-		// res.push(Math.min.apply(Math, arr.slice(n, (n + num))));
-		return Math.min.apply(Math, arr.slice(n, (n + num)));
-		}
-		// res.push(res);
-  // return res;
-}
-//////////////////////////////////////////////////////////////////////////////////
-    // rsvs.push((close - min) / (max - min) * 100);
-	// ks = sma(rsvs, 3)
-	// ds = sma(ks, 3)
-    // for (var i = 0; i < ks.length; i++) js.push(3 * ks[i] - 2 * ds[i]);
-//////////////////////////////////////////////////////////////////////////////////
-// console.log(max([5, 4, 9, 8, 789, 4578, 545, 785, 4589, 998], 3));
-// console.log(min([5, 4, 9, 8, 789, 4578, 545, 785, 4589, 998], 3));
-// console.log(sma([5, 4, 9, 8, 789, 4578, 545, 785, 4589, 998], 3));
